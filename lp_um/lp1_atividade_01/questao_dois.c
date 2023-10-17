60633c8919ad79328688a15aca48a15283271574
Raissa Carneiro <uefsraissa@gmail.com>
2023-10-17 08:11:23 -0300

    First commit

---
+++ lp_um/lp1_atividade_01/questao_dois.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a, c;
+    float x, b, d, y;
+    a = (2 + 3);
+    b = (4 + 5);
+    c = 6;
+    x = (c * a) / b;
+    d = (3 / 4);
+
+    y = (6 * 2) + d + 5;
+    printf("-------Letra a-------\n");
+    printf("O resultado da letra a e: %.2f\n",x);
+    printf("-------Letra b-------\n");
+    printf("O resultado da letra b e: %.2f\n",y);
+    return 0;
+
+
+}
+
+
---
+++ lp_um/lp1_atividade_01/questao_um.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Estou\n");
+    printf("testando o\n");
+    printf("printf.\n");
+    return 0;
+}
+
